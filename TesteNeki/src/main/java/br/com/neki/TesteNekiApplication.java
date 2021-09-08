8b17a3fb448e58b92aa6f2c9d82c64e7a6b3d5ae
Matheus M. Reis <mmr94@live.com>
2021-09-08 11:34:04 -0300

    Inicio do projeto e criação de pacotes

---
+++ TesteNeki/src/main/java/br/com/neki/TesteNekiApplication.java
@@ -0,0 +1,13 @@
+package br.com.neki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteNekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteNekiApplication.class, args);
+	}
+
+}
---
+++ TesteNeki/src/test/java/br/com/neki/TesteNekiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.neki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteNekiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
